ea08a864a4f0dd6f02f46a33bb900db1e5232c1c
Santhos kumar <52663775+santhoskumarayyappan@users.noreply.github.com>
2022-03-02 10:50:50 +0530

    Kth largest Integer in String Array

---
+++ KthLargestIntegerStringArray.java
@@ -0,0 +1,41 @@
+class KthLargestElementStringArray.java {
+    
+    public void swap(String[] nums, int i, int j) {
+        String temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public int partition(String[] nums, int l, int r) {
+        int pivotIndex = (int)(Math.random() * (r - l + 1));
+        swap(nums, r, l + pivotIndex);
+        for (int i = l; i < r; i++) {
+            if (compare(nums[r], nums[i]) > 0) {
+                swap(nums, i, l);
+                l++;
+            }
+        }
+        swap(nums, r, l);
+        return l;
+    }
+    
+    private int compare(String p1, String p2) {
+        return p1.length() == p2.length() ? p2.compareTo(p1) : Integer.compare(p2.length(), p1.length());
+    }
+    
+    public String kthLargestNumber(String[] nums, int k) {
+        int l = 0, r = nums.length - 1;
+        k--;
+        while(l <= r) {
+            int pivotIndex = partition(nums, l, r);
+            if (pivotIndex == k) {
+                return nums[k];
+            } else if (pivotIndex < k) {
+                l = pivotIndex + 1;
+            } else {
+                r = pivotIndex - 1;
+            }
+        }
+        return "";
+    }
+}
