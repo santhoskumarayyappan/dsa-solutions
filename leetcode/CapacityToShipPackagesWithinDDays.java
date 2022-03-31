d913fcf1a153a43bae19efc65e1df897fd85102b
Santhos kumar <52663775+santhoskumarayyappan@users.noreply.github.com>
2022-04-01 02:22:00 +0530

    Capacity To Ship Packages Within D Days

---
+++ leetcode/CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int getNoOfDaysToLoadforCapacity(int[] weights, int capacity) {
+        int days = 1, sum = 0;
+        for (int ele : weights) {
+            sum += ele;
+            if (sum > capacity) {
+                sum = ele;
+                days++;
+            } 
+        }
+        return days;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int left = -1, right = 0;
+        for (int ele : weights) {
+            right += ele;
+            left = Math.max(left, ele);
+        }
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int daysTaken = getNoOfDaysToLoadforCapacity(weights, mid);
+            if (daysTaken <= days) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
