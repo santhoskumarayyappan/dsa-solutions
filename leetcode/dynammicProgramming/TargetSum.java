dd2ae01ba6f7612f7e3945411cf2596bc7724a13
Santhos kumar <52663775+santhoskumarayyappan@users.noreply.github.com>
2022-04-16 12:33:54 +0530

    Target Sum

---
+++ leetcode/dynammicProgramming/TargetSum.java
@@ -0,0 +1,28 @@
+//16-04-2022
+//https://leetcode.com/problems/target-sum/
+
+
+
+
+// sum(P) - sum(N) = target
+// sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
+//                        2 * sum(P) = target + sum(nums)
+
+class Solution {
+    
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) / 2); 
+    }   
+
+    public int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1]; 
+        dp[0] = 1;
+        for (int n : nums)
+            for (int i = s; i >= n; i--)
+                dp[i] += dp[i - n]; 
+        return dp[s];
+    }
+}
