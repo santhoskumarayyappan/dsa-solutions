bfda2c17982f2630cb5e78ac2557c744f78e90a2
Santhos kumar <52663775+santhoskumarayyappan@users.noreply.github.com>
2022-04-22 03:24:41 +0530

    LeastRecentlyUsed

--- leetcode/design/LeastRecentlyUsed.java
+++ leetcode/design/LeastRecentlyUsed.java
@@ -20,3 +20,61 @@ class LRUCache extends LinkedHashMap<Integer, Integer> {
         return size() > capacity;
     }
 }
+
+
+// Without LinkedHashMap
+class LRUCache {
+    
+    private Map<Integer, Node> keyToNodeMap;
+    private LinkedList<Node> list;
+    private int capacity;
+    private Node head, tail;
+    
+    class Node {
+        int key;
+        int value;
+        
+        Node() {}
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public LRUCache(int capacity) {
+        keyToNodeMap = new HashMap<>();
+        list = new LinkedList<>();
+        this.capacity = capacity;
+    }
+    
+    public void moveToLast(Node node) {
+        list.remove(node);
+        list.addLast(node);        
+    }
+    
+    public int get(int key) {
+        if (keyToNodeMap.containsKey(key)) {
+            Node node = keyToNodeMap.get(key);
+            moveToLast(node);
+            return node.value;
+        } else {
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        if (keyToNodeMap.containsKey(key)) {
+            Node node = keyToNodeMap.get(key);
+            node.value = value;
+            moveToLast(node);
+        } else {
+            if (capacity == keyToNodeMap.size()) {
+                Node node = list.pollFirst();
+                keyToNodeMap.remove(node.key);
+            }
+            Node node = new Node(key, value);
+            list.addLast(node);
+            keyToNodeMap.put(key, node);
+        }
+    }
+}
