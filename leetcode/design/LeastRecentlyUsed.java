9110fda3a14601c50bec0a3be6928d218fc10f5c
Santhos kumar <52663775+santhoskumarayyappan@users.noreply.github.com>
2022-04-22 20:49:28 +0530

    Least Recently used

--- leetcode/design/LeastRecentlyUsed.java
+++ leetcode/design/LeastRecentlyUsed.java
@@ -22,17 +22,19 @@ class LRUCache extends LinkedHashMap<Integer, Integer> {
 }
 
 
-// Without LinkedHashMap
-class LRUCache {
+// Without LinkedHashMapclass LRUCache {
     
     private Map<Integer, Node> keyToNodeMap;
     private LinkedList<Node> list;
     private int capacity;
     private Node head, tail;
     
+
     class Node {
         int key;
         int value;
+        Node next;
+        Node prev;
         
         Node() {}
         Node(int key, int value) {
@@ -40,21 +42,53 @@ class LRUCache {
             this.value = value;
         }
     }
+    
 
     public LRUCache(int capacity) {
         keyToNodeMap = new HashMap<>();
         list = new LinkedList<>();
         this.capacity = capacity;
+        
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public void addLast(Node node) {
+        
+        node.next = tail;
+        node.prev = tail.prev;
+        
+        tail.prev.next = node;
+        tail.prev = node;
+        
+    }
+    
+    public Node remove(Node node) {
+        
+        Node next = node.next;
+        Node prev = node.prev;
+        
+        next.prev = prev;
+        prev.next = next;
+        
+        return node;        
+    }
+    
+    public Node pollFirst() {
+        return remove(head.next);
     }
     
     public void moveToLast(Node node) {
-        list.remove(node);
-        list.addLast(node);        
+        remove(node);
+        addLast(node);        
     }
     
     public int get(int key) {
         if (keyToNodeMap.containsKey(key)) {
-            Node node = keyToNodeMap.get(key);
+            Node node = keyToNodeMap.get(key);  
             moveToLast(node);
             return node.value;
         } else {
@@ -69,11 +103,11 @@ class LRUCache {
             moveToLast(node);
         } else {
             if (capacity == keyToNodeMap.size()) {
-                Node node = list.pollFirst();
+                Node node = pollFirst();
                 keyToNodeMap.remove(node.key);
             }
             Node node = new Node(key, value);
-            list.addLast(node);
+            addLast(node);
             keyToNodeMap.put(key, node);
         }
     }
