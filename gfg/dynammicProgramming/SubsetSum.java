8e3de0af89dabcb670ec600d26efe7b60d2a4179
Santhos kumar <52663775+santhoskumarayyappan@users.noreply.github.com>
2025-03-22 16:03:51 +0530

    Update SubsetSum.java

--- gfg/dynammicProgramming/SubsetSum.java
+++ gfg/dynammicProgramming/SubsetSum.java
@@ -4,7 +4,24 @@
 
 class Solution{
 
-
+    static Boolean isSubsetSum(int a[], int sum) {
+        int n = a.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        //dp[i][j] represents whether with subset of elements till 'i'  we can make sum 'j'  
+        dp[0][0] = true;
+        
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = true;
+            for (int j = 1; j <= sum; j++) {
+                if (j >= a[i - 1]) 
+                    dp[i][j] = dp[i - 1][j - a[i - 1]];
+                dp[i][j] |= dp[i - 1][j];
+            }
+        }
+        
+        return dp[n][sum];
+    }
+    
     static Boolean isSubsetSum(int N, int arr[], int sum){
         boolean[] subsetSum = new boolean[sum + 1];
         subsetSum[0] = true;
